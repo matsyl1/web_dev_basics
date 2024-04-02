ef9085bd56344809a38531ff5fc00b73e6417968
Mattias <herr_mattias@hotmail.com>
2024-04-02 18:39:38 +0200

    add nr guessing game

---
+++ nr_guessing_game/index.js
@@ -0,0 +1,34 @@
+// NR_GUESSING_GAME
+
+const min = 1;
+const max = 100;
+const answer = Math.floor(Math.random() * (max - min + 1)) + min;
+
+let attempts = 0;
+let guess;
+let running = true;
+
+while(running){
+    guess = window.prompt(`Guess a nr between ${min} - ${max}: `);
+    guess = Number(guess);
+    
+    if(isNaN(guess)){
+        window.alert("Enter a valid nr.");
+    }
+    else if(guess < min || guess > max){
+        window.alert(`Enter a nr between ${min} - ${max}: `);
+    }
+    else {
+        attempts++
+        if(guess < answer){
+            window.alert("Too low - try higher.");
+        }
+        else if(guess > answer){
+            window.alert("Too high - try lower.");
+        }
+        else{
+            window.alert(`BINGO! You guessed the correct nr ${answer} in ${attempts} attempts.`);
+            running = false;
+        }
+    }
+}
